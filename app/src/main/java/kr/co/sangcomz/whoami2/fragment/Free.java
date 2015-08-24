515803c20d3216afd647b85a563a163e7afe62e5
sangcomz <sangcomz@naver.com>
2015-08-25 01:01:15 +0900

    add

--- app/src/main/java/kr/co/sangcomz/whoami2/fragment/Free.java
+++ app/src/main/java/kr/co/sangcomz/whoami2/fragment/Free.java
@@ -1,8 +1,13 @@
 package kr.co.sangcomz.whoami2.fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +20,10 @@ import kr.co.sangcomz.whoami2.R;
 public class Free extends Fragment {
 //import android.support.v4.app.Fragment; 변경해줘야함
 
+    LinearLayoutManager linearLayoutManager;
+    RecyclerViewAdapter recyclerViewAdapter;
+    RecyclerView recyclerView;
+
     public Free() {
         // Required empty public constructor
     }
@@ -24,8 +33,63 @@ public class Free extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_free, container, false);
+        final View rv = inflater.inflate(
+                R.layout.fragment_free, container, false);
+        recyclerView = (RecyclerView) rv.findViewById(R.id.recyclerview);
+        linearLayoutManager = new LinearLayoutManager(recyclerView.getContext());
+        setupRecyclerView(recyclerView);
+
+        return rv;
     }
 
+    private void setupRecyclerView(RecyclerView recyclerView) {
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerViewAdapter = new RecyclerViewAdapter(getActivity());
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
 
+    public static class RecyclerViewAdapter
+            extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+        private final TypedValue mTypedValue = new TypedValue();
+        private int mBackground;
+        private Context context;
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+
+            public final View mView;
+            public final CardView cardView;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                cardView = (CardView) view.findViewById(R.id.card_view);
+            }
+        }
+        public RecyclerViewAdapter(Context context) {
+            context.getTheme().resolveAttribute(R.attr.selectableItemBackground, mTypedValue, true);
+            mBackground = mTypedValue.resourceId;
+            this.context = context;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.recycler_item, parent, false);
+            view.setBackgroundResource(mBackground);
+            return new ViewHolder(view);
+        }
+
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+//            holder.cardView
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
+    }
 }
